d2c2264aaec5c9c997950858a96e126c08b5031b
Thomas Randall <tlranda@clemson.edu>
2022-02-04 11:35:49 -0500

    Fix issues with guessing words that have repeating letters, clean up score reports, customize bias for words with unique letters

--- wordle.py
+++ wordle.py
@@ -46,18 +46,20 @@ def add_to_vocab(vocab, new_word, cache_target):
 			f.write(new_word+"\n")
 	return vocab
 
-def score(word, presence_dict, position_pres_dict):
+def score(word, presence_dict, position_pres_dict, unique_bias):
 	value = 0
 	for idx, letter in enumerate(word):
 		value = value + presence_dict[letter] + position_pres_dict[idx][letter]
+	# Bias towards having unique letters
+	if len(set(word)) == len(word):
+		value = value + unique_bias
 	return value
 
 # Initial vocabulary scoring based on letter-presence frequency
-def vocab_scoring(vocab, presence_weight=0, positional_weight=1):
+def vocab_scoring(vocab, presence_weight=0, positional_weight=1, unique_bias=1):
 	alphabet = dict((letter,0) for letter in [chr(ord('a')+_) for _ in range(26)])
 	# Discriminator in order should be based on POSITION as well
 	pos_alphabet = [dcpy(alphabet) for _ in range(5)]
-	uniques = []
 	for idx, word in enumerate(vocab):
 		# Count letter presence
 		wordset = set(word)
@@ -65,8 +67,6 @@ def vocab_scoring(vocab, presence_weight=0, positional_weight=1):
 			alphabet[letter] = alphabet[letter]+1
 		for idx, letter in enumerate(word):
 			pos_alphabet[idx][letter] = pos_alphabet[idx][letter]+1
-		if len(wordset) == len(word):
-			uniques.append(word)
 	# Normalize
 	norm_factor = presence_weight * len(vocab)
 	if norm_factor != 0:
@@ -84,10 +84,9 @@ def vocab_scoring(vocab, presence_weight=0, positional_weight=1):
 	  for idx in range(5):
 		  for letter in pos_alphabet[idx].keys():
 			  pos_alphabet[idx][letter] = 0
-	# Score uniques
-	unq2scr = dict((word, score(word, alphabet, pos_alphabet)) for word in uniques)
-	# Change unq2scr into OrderedDict on descending score
-	return alphabet, pos_alphabet, unq2scr, uniques
+	# Score all words
+	word2scr = dict((word, score(word, alphabet, pos_alphabet, unique_bias)) for word in vocab)
+	return alphabet, pos_alphabet, word2scr
 
 def make_guess(scoredict, vocab, correct, scoreWeight):
 	# Make max-guess
@@ -121,30 +120,33 @@ def main(args):
 	vocab_size = []
 	history = []
 	guess_score = []
-	for attempt in range(args.guess_limit):
+	for attempt in range(1, 1+args.guess_limit):
 		vocab_size.append(len(vocab))
-		alphabet, pos_alphabet, unq2scr, uniques = vocab_scoring(vocab, args.presence_weight, args.position_weight)
-		"""
-		maxscore = max(unq2scr.values())
-		has_max = [key for (key, value) in unq2scr.items() if value == maxscore]
-		print(f"Max score is {maxscore}, held by {len(has_max)} words; exemplar: {has_max[0]} {has_max}")
-		"""
-		vocab, guess, score = make_guess(unq2scr, vocab, args.solution, args.scoreWeight)
+		alphabet, pos_alphabet, word2scr = vocab_scoring(vocab, args.presence_weight, args.position_weight, args.unique_bias)
+		vocab, guess, score = make_guess(word2scr, vocab, args.solution, args.scoreWeight)
 		history.append(guess)
 		guess_score.append(score)
+		print(f"{attempt}/{args.guess_limit} = {guess}: {score} [1/{vocab_size[-1]}]")
 		if guess == args.solution:
 			break
-	for g,s,v in zip(history, guess_score, vocab_size):
-		print(f"{g}: {s} [1/{v}]")
+	if guess != args.solution:
+		print(f"OUT OF GUESSES. {len(vocab)} words remained")
+		char = ''
+		while char not in ['y', 'n']:
+			print("Show remaining words? y/n: ")
+			char = input()
+		if char == 'y':
+			print(", ".join(vocab))
 
 def build():
 	prs = argparse.ArgumentParser(description='Dynamic Wordle Solver')
 	prs.add_argument('--no_cache', dest='cached', action='store_false', help='Do not use cached vocabulary')
-	prs.add_argument('--cache_file', dest='cache_target', type=str, default='5lw.txt', help='Filepath to cached vocabulary')
-	prs.add_argument('--presence_weight', '--presence', type=float, default=1, help='Weight assigned to letters APPEARING in a word')
-	prs.add_argument('--position_weight', '--position', type=float, default=1, help='Weight assigned to order of letters that appear in a word')
-	prs.add_argument('--guess_limit', '--guess', type=int, default=6, help='Maximum number of allowed guesses')
-	prs.add_argument('--solution', '--word', type=str, help='Correct word to guess', default='pleat')
+	prs.add_argument('--cache_file', dest='cache_target', type=str, default='5lw.txt', help='Filepath to cached vocabulary (default 5lw.txt)')
+	prs.add_argument('--presence_weight', '--presence', type=float, default=1, help='Weight assigned to letters APPEARING in a word (default 1)')
+	prs.add_argument('--position_weight', '--position', type=float, default=1, help='Weight assigned to order of letters that appear in a word (default 1)')
+	prs.add_argument('--unique_bias', '--unique', type=float, default=1, help='Bias weight towards words that have all unique letters (default 1)')
+	prs.add_argument('--guess_limit', '--guess', type=int, default=6, help='Maximum number of allowed guesses (default 6)')
+	prs.add_argument('--solution', '--word', type=str, help='Correct word to guess (default guess)', default='guess')
 	prs.add_argument('--not_present_score' '--np_score', dest='npscore', type=float, default=1, help='Entropy score for guessing a letter not present in the solution (default: 1pts)')
 	prs.add_argument('--present_score' '--p_score', dest='pscore', type=float, default=2, help='Entropy score for guessing a letter present in the solution, but in the wrong space (default: 2pts)')
 	prs.add_argument('--correct_score' '--c_score', dest='cscore', type=float, default=5, help='Entropy score for guessing a letter correctly from the solution (default: 5pts)')
@@ -156,6 +158,5 @@ def parse(prs, args=None):
 	return args
 
 if __name__ == '__main__':
-	args = parse(build())
-	main(args)
+	main(parse(build()))
 

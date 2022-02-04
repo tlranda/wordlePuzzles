d71053cab84b37276dc886640ed1f5b314af53d5
Thomas Randall <tlranda@clemson.edu>
2022-02-04 11:10:24 -0500

    Finish draft script: * Allow new solution words to be added to cache list * Makes guesses using maxscore strategy * Updates output to show attempts * Arguments now allow:   + Guess Limitation adjustment   + User-provided solution words   + User-defined entropy scores (not used in guess-making process)

--- wordle.py
+++ wordle.py
@@ -18,7 +18,6 @@ def load_vocab(cached=True, cache_target='5lw.txt'):
 	if cached and os.path.exists(cache_target):
 		with open(cache_target, 'r') as words:
 			vocab = [_.rstrip() for _ in words.readlines()]
-			print(len(vocab))
 	else:
 		# Gracefully navigate partial setups, informing as best as possible
 		try:
@@ -33,7 +32,18 @@ def load_vocab(cached=True, cache_target='5lw.txt'):
 			raise e
 		vocab = [_.lower() for _ in words if len(_) == 5]
 		with open(cache_target, 'w') as f:
-			f.write("\n".join(vocab))
+			f.write("\n".join(vocab)+"\n")
+	return vocab
+
+def add_to_vocab(vocab, new_word, cache_target):
+	char = ''
+	while char not in ['y', 'n']:
+		print(f"Vocabulary does not know the word '{args.solution}' and will be unable to guess it. Learn it? y/n: ")
+		char = input()
+	if char == 'y':
+		vocab.append(new_word)
+		with open(cache_target, 'a') as f:
+			f.write(new_word+"\n")
 	return vocab
 
 def score(word, presence_dict, position_pres_dict):
@@ -77,14 +87,55 @@ def vocab_scoring(vocab, presence_weight=0, positional_weight=1):
 	# Score uniques
 	unq2scr = dict((word, score(word, alphabet, pos_alphabet)) for word in uniques)
 	# Change unq2scr into OrderedDict on descending score
-	maxscore = max(unq2scr.values())
-	has_max = [key for (key, value) in unq2scr.items() if value == maxscore]
-	print(f"Max score is {maxscore}, held by {len(has_max)} words; exemplar: {has_max[0]} {has_max}")
-	return alphabet, unq2scr, uniques
+	return alphabet, pos_alphabet, unq2scr, uniques
+
+def make_guess(scoredict, vocab, correct, scoreWeight):
+	# Make max-guess
+	maxscore = max(scoredict.values())
+	candidates = [key for (key, value) in scoredict.items() if value == maxscore]
+	guess = candidates[0]
+	guess_score = 0
+	# Learn from guess
+	# Remove words containing a letter we learned wasn't present
+	invalid_letters = [_ for _ in guess if _ not in correct]
+	guess_score += scoreWeight[0] * len(invalid_letters)
+	for bad_letter in invalid_letters:
+		vocab = [_ for _ in vocab if bad_letter not in _]
+	# Remove words with a mispositioned letter and those that lack the letter since it should be somewhere
+	almost_position = [_ for _ in guess if _ in correct and correct.index(_) != guess.index(_)]
+	for almost_letter in almost_position:
+		vocab = [_ for _ in vocab if almost_letter in _ and _.index(almost_letter) != guess.index(almost_letter)]
+	guess_score += scoreWeight[1] * len(almost_position)
+	# Remove words that don't have the correct letter in the correct spot
+	correct_position = [_ for _ in guess if _ in correct and correct.index(_) == guess.index(_)]
+	for correct_letter in correct_position:
+		vocab = [_ for _ in vocab if correct_letter in _ and _.index(correct_letter) == guess.index(correct_letter)]
+	guess_score += scoreWeight[2] * len(correct_position)
+	return vocab, guess, guess_score
 
 def main(args):
 	vocab = load_vocab(args.cached, args.cache_target)
-	vocab_scoring(vocab, args.presence_weight, args.position_weight)
+	# Anti-cheat check
+	if args.solution not in vocab:
+		vocab = add_to_vocab(vocab, args.solution, args.cache_target)
+	vocab_size = []
+	history = []
+	guess_score = []
+	for attempt in range(args.guess_limit):
+		vocab_size.append(len(vocab))
+		alphabet, pos_alphabet, unq2scr, uniques = vocab_scoring(vocab, args.presence_weight, args.position_weight)
+		"""
+		maxscore = max(unq2scr.values())
+		has_max = [key for (key, value) in unq2scr.items() if value == maxscore]
+		print(f"Max score is {maxscore}, held by {len(has_max)} words; exemplar: {has_max[0]} {has_max}")
+		"""
+		vocab, guess, score = make_guess(unq2scr, vocab, args.solution, args.scoreWeight)
+		history.append(guess)
+		guess_score.append(score)
+		if guess == args.solution:
+			break
+	for g,s,v in zip(history, guess_score, vocab_size):
+		print(f"{g}: {s} [1/{v}]")
 
 def build():
 	prs = argparse.ArgumentParser(description='Dynamic Wordle Solver')
@@ -92,10 +143,16 @@ def build():
 	prs.add_argument('--cache_file', dest='cache_target', type=str, default='5lw.txt', help='Filepath to cached vocabulary')
 	prs.add_argument('--presence_weight', '--presence', type=float, default=1, help='Weight assigned to letters APPEARING in a word')
 	prs.add_argument('--position_weight', '--position', type=float, default=1, help='Weight assigned to order of letters that appear in a word')
+	prs.add_argument('--guess_limit', '--guess', type=int, default=6, help='Maximum number of allowed guesses')
+	prs.add_argument('--solution', '--word', type=str, help='Correct word to guess', default='pleat')
+	prs.add_argument('--not_present_score' '--np_score', dest='npscore', type=float, default=1, help='Entropy score for guessing a letter not present in the solution (default: 1pts)')
+	prs.add_argument('--present_score' '--p_score', dest='pscore', type=float, default=2, help='Entropy score for guessing a letter present in the solution, but in the wrong space (default: 2pts)')
+	prs.add_argument('--correct_score' '--c_score', dest='cscore', type=float, default=5, help='Entropy score for guessing a letter correctly from the solution (default: 5pts)')
 	return prs
 
 def parse(prs, args=None):
 	args = prs.parse_args(args)
+	args.scoreWeight = [args.npscore, args.pscore, args.cscore]
 	return args
 
 if __name__ == '__main__':
